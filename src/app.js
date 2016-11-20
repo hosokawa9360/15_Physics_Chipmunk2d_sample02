ddb163e15c95455398b7b47b8e83abbd2f7fc70f
Junya Hosokawa <hosokawa@morijyobi.com>
2016-11-20 18:36:40 +0900

    setDefaultCollisionHandlerで実装
    addCollisionHandlerでは物体をすりぬけてしまうバグがあるので
    setDefaultCollosionHandlerで実装

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 var world;
 var shapeArray=[];
 
+if (typeof SpriteTag == "undefined") {
+   var SpriteTag = {};
+   SpriteTag.totem = 0; // トーテム
+   SpriteTag.destroyable = 1; //
+   SpriteTag.solid = 2; //
+   SpriteTag.ground = 3; //地面
+
+};
+var gameLayer;
 var gameScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
@@ -29,17 +38,29 @@ var game = cc.Layer.extend({
         world.addStaticShape(wallBottom);
 
 
-        this.addBody(240,10,480,20,false,res.ground_png,"ground");
-        this.addBody(204,32,24,24,true,res.brick1x1_png,"destroyable");
-        this.addBody(276,32,24,24,true,res.brick1x1_png,"destroyable");
-        this.addBody(240,56,96,24,true,res.brick4x1_png,"destroyable");
-        this.addBody(240,80,48,24,true,res.brick2x1_png,"solid");
-        this.addBody(228,104,72,24,true,res.brick3x1_png,"destroyable");
-        this.addBody(240,140,96,48,true,res.brick4x2_png,"solid");
-        this.addBody(240,188,24,48,true,res.totem_png,"totem");
+        // this.addBody(240,10,480,20,false,res.ground_png,"ground");
+        // this.addBody(204,32,24,24,true,res.brick1x1_png,"destroyable");
+        // this.addBody(276,32,24,24,true,res.brick1x1_png,"destroyable");
+        // this.addBody(240,56,96,24,true,res.brick4x1_png,"destroyable");
+        // this.addBody(240,80,48,24,true,res.brick2x1_png,"solid");
+        // this.addBody(228,104,72,24,true,res.brick3x1_png,"destroyable");
+        // this.addBody(240,140,96,48,true,res.brick4x2_png,"solid");
+        // this.addBody(240,188,24,48,true,res.totem_png,"totem");
+
+        this.addBody(240,10,480,20,false,res.ground_png,SpriteTag.ground);
+        this.addBody(204,32,24,24,true,res.brick1x1_png,SpriteTag.destroyable);
+        this.addBody(276,32,24,24,true,res.brick1x1_png,SpriteTag.destroyable);
+        this.addBody(240,56,96,24,true,res.brick4x1_png,SpriteTag.destroyable);
+        this.addBody(240,80,48,24,true,res.brick2x1_png,SpriteTag.solid);
+        this.addBody(228,104,72,24,true,res.brick3x1_png,SpriteTag.destroyable);
+        this.addBody(240,140,96,48,true,res.brick4x2_png,SpriteTag.solid);
+        this.addBody(240,188,24,48,true,res.totem_png,SpriteTag.totem);
+
+
         this.scheduleUpdate();
         cc.eventManager.addListener(touchListener, this);
         world.setDefaultCollisionHandler (this.collisionBegin,null,null,null);
+
     },
     addBody: function(posX,posY,width,height,isDynamic,spriteImage,type){
         if(isDynamic){
@@ -59,6 +80,7 @@ var game = cc.Layer.extend({
         shape.setFriction(1);
         shape.setElasticity(0);
         shape.name=type;
+        shape.setCollisionType(type);
         shape.image=bodySprite;
         world.addShape(shape);
         shapeArray.push(shape);
@@ -73,19 +95,66 @@ var game = cc.Layer.extend({
         }
     },
     collisionBegin : function (arbiter, space ) {
+      /*
         if((arbiter.a.name=="totem" && arbiter.b.name=="ground") || (arbiter.b.name=="totem" && arbiter.a.name=="ground")){
             console.log("Oh no!!!!");
         }
+        */
+
+        if(arbiter.a.name== SpriteTag.totem && arbiter.b.name== SpriteTag.ground ) {
+           cc.audioEngine.playEffect(res.landing_mp3);
+        }
         return true;
-    }
+    },
+
+
 });
 
+
+var touchListener = cc.EventListener.create({
+   event: cc.EventListener.TOUCH_ONE_BY_ONE, // シングルタッチのみ対応
+   swallowTouches: false, // 以降のノードにタッチイベントを渡す
+   onTouchBegan: function(touch, event) { // タッチ開始時
+      var pos = touch.getLocation();
+
+      console.log("shapeArray.length:", shapeArray.length)
+         // すべてのshapをチェックする
+      for (var i = 0; i < shapeArray.length; i++) {
+         var shape = shapeArray[i];
+         console.log("shape.type:", i, shape.type)
+            //pointQueryは物理オブジェクトの内側がタップされたかどうか判定する関数
+         if (shape.pointQuery(cp.v(pos.x, pos.y)) != undefined) {
+            console.log("hit ")
+            if (shape.name == SpriteTag.destroyable) {
+               //ブロックをタップしたときは、消去する
+               world.removeBody(shape.getBody());
+               world.removeShape(shape);
+               gameLayer.removeChild(shape.image);
+               shapeArray.splice(i, 1);
+               console.log("remove block")
+               return;
+            } else if (shape.name == SpriteTag.totem) {
+               // トーテムをタップしたときは、衝撃を与える
+               shape.body.applyImpulse(cp.v(500, 0), cp.v(0, -20))
+               return;
+            }
+         }
+      }
+      // 何も無い場所をタップしたときは箱を追加する
+      gameLayer.addBody(pos.x,pos.y,24,24,true,res.brick1x1_png,SpriteTag.destroyable);
+      return;
+
+   }
+
+});
+
+/*
 var touchListener = cc.EventListener.create({
     event: cc.EventListener.TOUCH_ONE_BY_ONE,
     onTouchBegan: function (touch, event) {
         for(var i=shapeArray.length-1;i>=0;i--){
             if(shapeArray[i].pointQuery(cp.v(touch.getLocation().x,touch.getLocation().y))!=undefined){
-                if(shapeArray[i].name=="destroyable"){
+                if(shapeArray[i].name== SpriteTag.destroyable ){
                     gameLayer.removeChild(shapeArray[i].image);
                     world.removeBody(shapeArray[i].getBody())
                     world.removeShape(shapeArray[i])
@@ -95,3 +164,4 @@ var touchListener = cc.EventListener.create({
         }
     }
 });
+*/
